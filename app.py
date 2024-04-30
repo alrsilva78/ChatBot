9d5ca694adb7c20ddc20238e326497e85359006d
Oraclle78 <informaticaalrsilva@hotmail.com>
2024-04-30 20:00:27 -0300

    app v1

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
+
+RASA_API_URL = "http://localhost:5002/api"
 
 app= Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template ('index.html')
-   #return "Esse e o meu site"
+
+@app.route('/api', method=['POST'])
+def webhook():
+    user_message = request.json['message']
+    print("User Message:", user_message)
+    
+    # Send user message to Tasa and get bot's response
+    rasa_response = request.post(RASA_API_URL, json={'messsage': user_message})
+    rasa_response_json = rasa_response.json()
+    
+    print("Rasa Response:", rasa_response_json)
+    
+    bot_response = rasa_response_json[0]['text'] if rasa_response else 'Sorry I didn\'t understand that.'
+    
+    return jsonify({'response': bot_response})
+   
 
 if __name__ == "__main__":
     app.run(debug = True, port= 3000)
